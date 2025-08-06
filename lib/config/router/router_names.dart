dea9e1fe5a66128b69870335c5607e271dbf51a0
Hanife Ercan <102989222+hanifeercan@users.noreply.github.com>
2025-08-06 16:09:50 +0300

    Create router provider

---
+++ lib/config/router/router_names.dart
@@ -0,0 +1,12 @@
+class RouteNames {
+  static const home = 'home';
+  static const signin = 'signin';
+  static const signup = 'signup';
+  static const resetPassword = 'resetPassword';
+  static const verifyEmail = 'verifyEmail';
+  static const changePassword = 'changePassword';
+  static const splash = 'splash';
+  static const firebaseError = 'firebaseError';
+  static const newPhotoNote = 'newPhotoNote';
+  static const detailPhotoNote = 'detailPhotoNote';
+}
